742d2845d331aec97d2589f680a4353fe86fadde
Zionek <128070406+Zionekkk@users.noreply.github.com>
2024-12-24 23:47:13 +0100

    blacklisted.lua update

--- Blacklisted.lua
+++ Blacklisted.lua
@@ -1,1 +1,13 @@
-function fetchlist(id) end
+local blacklisted = {
+    "1",
+    -- add more
+} -- Make serversided, for now client-sided
+
+function fetchlist(id)
+    for i,v in pairs(blacklisted) do
+        if v == id then
+            return true
+        end
+    end
+    return false
+end
