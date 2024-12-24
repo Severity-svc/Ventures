b03ab8bafd81a7edfda66040a87a117ab484ac06
Severity <system0464@gmail.com>
2024-12-24 16:11:54 +0200

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -14,20 +14,5 @@ local function fetchGame(id)
     end
 end
 
-local id = game.PlaceId
-local urls = setmetatable({
-    [1] = "https://raw.githubusercontent.com/Severity-svc/Ventures/refs/heads/main/Profiles/16732694052%20.lua?token=GHSAT0AAAAAAC4LMTZ7KT56KHDL5EV7BLOYZ3KXZ3A",
-}, {
-    __index = function()
-        return nil
-    end
-})
-
-local gameKey = fetchGame(id)
-
-if gameKey then
-    local url = urls[gameKey]
-    if url then
-	loadstring(game:HttpGet(tostring(url)))()
-    end
-end
+local key = fetchGame(game.PlaceID)
+print(tostring(key))
