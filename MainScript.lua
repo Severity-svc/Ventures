6b114f8f539d016cd923393f5680f83f3b93cb03
Severity <system0464@gmail.com>
2024-12-24 16:18:41 +0200

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -15,4 +15,12 @@ local function fetchGame(id)
 end
 
 local key = fetchGame(game.PlaceId)
-print(tostring(key))
+
+if key then
+ if key == 1 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/Severity-svc/Ventures/refs/heads/main/Profiles/16732694052%20.lua?token=GHSAT0AAAAAAC4LMTZ7OF4T7EVTWIRG35HGZ3KYK7A'))()
+  end
+else
+    warn("game not supported")
+end
+
