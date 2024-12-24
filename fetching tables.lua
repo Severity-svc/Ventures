f1016c793482b5000a2b11059fe2c94bb113c39c
Severity <system0464@gmail.com>
2024-12-24 18:04:36 +0200

    Create fetching tables.lua

---
+++ fetching tables.lua
@@ -0,0 +1,10 @@
+local function gs(name:String)
+    return game:GetService(name)
+end
+
+local fetch = {
+  run = gs("RunService"),
+  uis = gs("UserInputService"),
+  http = gs("HttpService"),  
+  tween = gs("TweenService"),
+}
