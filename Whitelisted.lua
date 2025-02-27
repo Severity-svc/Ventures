3044fdef8fc244127854d7b598a7a1bc0061bdda
Severity <system0464@gmail.com>
2025-02-27 11:38:50 +0200

    Update Whitelisted.lua

--- Whitelisted.lua
+++ Whitelisted.lua
@@ -1,6 +1,7 @@
 local whitelisted = {
 	["Severity"] = {
 		["Rank"] = "Owner",
+		["RankColor"] = Color3.fromRGB(255, 43, 255),
 		["ID"] = "240692f-e72-11ef-9c32-806e6f6e6963E234A00318E64546996ECC216EEDAC75",
 	},
 }
