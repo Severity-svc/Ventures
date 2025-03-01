e2018a31def734bd7d57bae82347e18b29f4cf85
Severity <system0464@gmail.com>
2025-03-01 21:31:35 +0200

    Update Whitelisted.lua

--- Whitelisted.lua
+++ Whitelisted.lua
@@ -4,6 +4,12 @@ local whitelisted = {
 		["RankColor"] = Color3.fromRGB(255, 43, 255),
 		["ID"] = "E234A003-18E6-4546-996E-CC216EEDAC75",
 	},
+
+	["Retard"] = {
+		["Rank"] = "Beta Tester (slave),
+		["RankColor"] = Color3.fromRGB(255,255,255),
+		["ID"] = "D0541B82-1DD5-46BA-9FAD-FD98DDBCC750"
+	},
 }
 
 
