a42d8bdf24914d918d7d59a1bee748b6c7e45a45
Severity <system0464@gmail.com>
2025-03-22 23:02:52 +0200

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,41 +1,22 @@
-local lib = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local Game = tostring(game.PlaceId)
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Severity-svc/Ventures/refs/heads/main/Gui/NewGuiLibrary/GuiLibrary.lua'))()
 
-local function fetchgame(id)
-    local supported = setmetatable({
-        [1] = 16732694052,
-        [2] = 17625359962,
-	[3] = 301549746,
-    }, {
-        __index = function()
-            return nil
-        end
-    })
+local Branch = "https://raw.githubusercontent.com/"
+local HostName = "Severity-svc/"
+local GlobalExtension = ".lua"
+local Asserted = Branch .. HostName.. "/Ventures/".. "/refs/heads/main/".. "Profiles/".. Game .. GlobalExtension
 
-    for k, v in ipairs(supported) do
-        if v == id then
-            return k
-        end
-    end
-end
+local Success, Err = pcall(function()
+	loadstring(game:HttpGet(Asserted))()
+	if getgenv and getgenv().queue_on_teleport then
+		getgenv().queue_on_teleport('loadstring(game:HttpGet("' .. Asserted .. '"))()')
+	end
+end)
 
-local key = fetchgame(game.PlaceId)
-setclipboard("discord.gg/v3n")
-
-if key then
- if key == 1 then
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/Severity-svc/Ventures/refs/heads/main/Profiles/16732694052%20.lua'))()
-    elseif key == 2 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Severity-svc/Ventures/refs/heads/main/Profiles/17625359962.lua"))()
-	elseif key == 3 then
- 	 loadstring(game:HttpGet("https://raw.githubusercontent.com/Severity-svc/Ventures/refs/heads/main/Profiles/301549746.lua"))()
-  end
-else
-    lib:Notify({
-        Title = "Ventures",
-		Content = "Game Not Supported!",
-		SubContent = "",
-		Duration = 3 
+if not Success then
+	Library:CreateNotification({
+		Title = "Ventures - Unsupported Game",
+		Content = "This Game Is Not Supported",
+		Duration = 4,
 	})
 end
-
- queue_on_teleport("https://raw.githubusercontent.com/Severity-svc/Ventures/refs/heads/main/MainScript.lua") 
