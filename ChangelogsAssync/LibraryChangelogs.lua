126d675458bc035abdade27a22fe343e8813e68f
Severity <system0464@gmail.com>
2025-03-23 16:13:07 +0200

    Update LibraryChangelogs.lua

--- ChangelogsAssync/LibraryChangelogs.lua
+++ ChangelogsAssync/LibraryChangelogs.lua
@@ -1,40 +1,42 @@
-local Write = {
-    {
-        Name = "Library | Changelogs | 23.03.2025",
-        FixHeader = {
-            Title = "Fixes:",
-            Content = [[
+local -- This is the script hosted on GitHub
+Write = {
+    Paragraph = { 
+        {
+            Name = "Library | Changelogs | 23.03.2025",
+            FixHeader = {
+                Title = "Fixes:",
+                Content = [[
 [+] Fixed Notification Animation Going Inside Other Notifications
 [+] Fixed Keysystem Minimize Icon
 [+] Fixed Keybind Bugging Out
 [+] Fixed ScrollingFrame Sizing
 ]],
-            TitleAccentColor = Color3.fromRGB(241, 241, 241),
-            ContentAccentColor = Color3.fromRGB(206, 206, 206),
-            AccentColor = Color3.fromRGB(24, 52, 66),
-            AccentTransparency = 0,
-            StrokeAccentColor = Color3.fromRGB(62, 134, 170),
-            UseShadowGrandient = { Enabled = true },
-        },
-        AddedHeader = {
-            Title = "Added:",
-            Content = [[
+                TitleAccentColor = Color3.fromRGB(241, 241, 241),
+                ContentAccentColor = Color3.fromRGB(206, 206, 206),
+                AccentColor = Color3.fromRGB(24, 52, 66),
+                AccentTransparency = 0,
+                StrokeAccentColor = Color3.fromRGB(62, 134, 170),
+                UseShadowGrandient = { Enabled = true },
+            },
+            AddedHeader = {
+                Title = "Added:",
+                Content = [[
 [+] Added Paragraphs
 [+] Added ChangeLogs
 [+] Added Mobile Device Detection
 [+] Added Headers And HeaderIcons
 [+] Added KeySystem TextBox Enter Animation
 ]],
-            TitleAccentColor = Color3.fromRGB(241, 241, 241),
-            ContentAccentColor = Color3.fromRGB(206, 206, 206),
-            AccentColor = Color3.fromRGB(37, 23, 66),
-            AccentTransparency = 0,
-            StrokeAccentColor = Color3.fromRGB(98, 61, 176),
-            UseShadowGrandient = { Enabled = true },
-        },
-        ChangedHeader = {
-            Title = "Changed:",
-            Content = [[
+                TitleAccentColor = Color3.fromRGB(241, 241, 241),
+                ContentAccentColor = Color3.fromRGB(206, 206, 206),
+                AccentColor = Color3.fromRGB(37, 23, 66),
+                AccentTransparency = 0,
+                StrokeAccentColor = Color3.fromRGB(98, 61, 176),
+                UseShadowGrandient = { Enabled = true },
+            },
+            ChangedHeader = {
+                Title = "Changed:",
+                Content = [[
 [+] Changed MainGui Drag Speed
 [+] Changed Slider Accent Color
 [+] Changed Notification Size And Font
@@ -44,12 +46,13 @@ local Write = {
 [+] Changed Design Style (Inspired By RayField :3)
 [+] Changed Keysystem Exit Animation
 ]],
-            TitleAccentColor = Color3.fromRGB(241, 241, 241),
-            ContentAccentColor = Color3.fromRGB(206, 206, 206),
-            AccentColor = Color3.fromRGB(66, 54, 24),
-            AccentTransparency = 0,
-            StrokeAccentColor = Color3.fromRGB(141, 114, 51),
-            UseShadowGrandient = { Enabled = true },
+                TitleAccentColor = Color3.fromRGB(241, 241, 241),
+                ContentAccentColor = Color3.fromRGB(206, 206, 206),
+                AccentColor = Color3.fromRGB(66, 54, 24),
+                AccentTransparency = 0,
+                StrokeAccentColor = Color3.fromRGB(141, 114, 51),
+                UseShadowGrandient = { Enabled = true },
+            }
         }
     }
 }
