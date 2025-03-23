d5f722e060d7ab6afedddfdc9fee3208fabc7268
Severity <system0464@gmail.com>
2025-03-23 14:06:06 +0200

    Update LibraryChangelogs.lua

--- ChangelogsAssync/LibraryChangelogs.lua
+++ ChangelogsAssync/LibraryChangelogs.lua
@@ -1,39 +1,43 @@
-local Paragraph = ChangeLogs:CreateParagraph({
-	Name = "Changelogs",
+	local ChangeLogs = Tabs:CreateTab({
+				Name = "Changelogs",
+				Icon = "pen",
+			})
 
-	FixHeader = {
-		Title = "Discord",
-		Content = "Join Our Discord Server For More Updates And Key Changes!",
+			local Paragraph = ChangeLogs:CreateParagraph({
+				Name = "Changelogs",
 
-		TitleAccentColor = Color3.fromRGB(255, 255, 255),
-		ContentAccentColor = Color3.fromRGB(255, 255, 255),
+				FixHeader = {
+					Title = "Discord",
+					Content = "Join Our Discord Server For More Updates And Key Changes!",
 
-		AccentColor = Color3.fromRGB(165, 165, 255),
-		AccentTransparency = 0.8,
-		StrokeAccentColor = Color3.fromRGB(165, 165, 255),
-		
-		UseShadowGrandient = {
-			Enabled = true
-		},
+					TitleAccentColor = Color3.fromRGB(255, 255, 255),
+					ContentAccentColor = Color3.fromRGB(255, 255, 255),
 
-		CustomImage = {
-			Icon = "http://www.roblox.com/asset/?id=84828491431270",
-			BackgroundTransparency = 0.8,
-			BackgroundColor = Color3.fromRGB(255, 255, 255),
+					AccentColor = Color3.fromRGB(165, 165, 255),
+					AccentTransparency = 0.8,
+					StrokeAccentColor = Color3.fromRGB(165, 165, 255),
 
-			Callback = function()
-				print("alalabua")
-			end,
+					UseShadowGrandient = {
+						Enabled = true
+					},
 
-			Stroke = {
-				Enabled = true,
-				Color = Color3.fromRGB(182, 166, 204),
-				Thickness = 1,
-				Transparency = 0
-			}
-		}
-	},
-})
+					CustomImage = {
+						Icon = "http://www.roblox.com/asset/?id=84828491431270",
+						BackgroundTransparency = 0.8,
+						BackgroundColor = Color3.fromRGB(255, 255, 255),
 
+						Callback = function()
+							print("alalabua")
+						end,
 
-return Paragraph
+						Stroke = {
+							Enabled = true,
+							Color = Color3.fromRGB(182, 166, 204),
+							Thickness = 1,
+							Transparency = 0
+						}
+					}
+				},
+			})
+
+return ChangeLogs
